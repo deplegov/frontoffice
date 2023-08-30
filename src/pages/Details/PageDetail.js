96c07201a6c44628f74f060a6859749d4d128ca0
jheri04 <139941100+jheri04@users.noreply.github.com>
2023-08-31 00:28:37 +0300

    integration details tender

--- src/pages/Details/PageDetail.js
+++ src/pages/Details/PageDetail.js
@@ -6,162 +6,215 @@ import NavBarInitialDefault from "components/NavBarInitialDefault";
 import bgImage from "assets/images/bg-presentation.jpg";
 import DefaultFooter from "examples/Footers/DefaultFooter";
 import footerRoutes from "footer.routes";
-import MKPagination from "components/MKPagination";
-import { Icon } from "@mui/material";
+// import MKPagination from "components/MKPagination";
+// import { Icon } from "@mui/material";
 import PropTypes from "prop-types";
 import MKInput from "components/MKInput";
 import MKButton from "components/MKButton";
 import CommentLine from "components/CommentLine/commentLine";
-import { useState } from "react";
-import Reacts from "components/Reacts/reacts";
+import { useEffect, useState } from "react";
+// import Reacts from "components/Reacts/reacts";
+import { useParams } from "react-router-dom";
+import api from "utils/api";
 
 PageDetail.propTypes = {
   page: PropTypes.string,
 };
 
 export default function PageDetail() {
-  let commentData = {
-    username: "feno.rak",
-    date: "21 Juil 2023, 21:30",
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet diam necjusto finibus dictum.",
+  const logged = !(sessionStorage.getItem("user") === null);
+
+  const { id } = useParams();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [countComments, setCountComments] = useState(0);
+  const [detail, setDetail] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getDetails(() => getComments(getCountComments));
+    if (logged) setUser(JSON.parse(sessionStorage.getItem("user")));
+  }, []);
+
+  const addComment = () => {
+    fetch(api("comments"), {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({
+        contenu: comment,
+        utilisateur_id: user.ID,
+        article_id: id,
+      }),
+    }).then((res) => {
+      if (res.ok)
+        res.json().then(() => {
+          const tmp = comments;
+          tmp.push({
+            CONTENU: comment,
+            DATE_COM: new Date(),
+            UTILISATEUR_ID: user.ID,
+            ARTICLE_ID: id,
+            UTILISATEUR: `${user.NOM} ${user.PRENOM}`,
+          });
+          const count = countComments + 1;
+          setCountComments(count);
+          setComments(tmp);
+          setComment("");
+        });
+    });
   };
 
-  const [comment, setComment] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [logged, setLogged] = useState(true);
+  const getDetails = (callback) => {
+    fetch(api(`articles/${id}`)).then((res) => {
+      if (res.ok) {
+        return res.json().then((data) => {
+          if (data && data.length > 0) setDetail(data[0]);
+          callback();
+        });
+      }
+    });
+  };
+
+  const getComments = (callback) => {
+    fetch(api(`comments?article_id=${id}`)).then((res) => {
+      if (res.ok) {
+        return res.json().then((data) => {
+          setComments(data);
+          callback();
+        });
+      }
+    });
+  };
+
+  const getCountComments = () => {
+    fetch(api(`comments/count?article_id=${id}`)).then((res) => {
+      if (res.ok) {
+        return res.json().then((data) => {
+          setCountComments(data);
+          setLoading(false);
+        });
+      }
+    });
+  };
 
   const onComment = () => {
-    console.log(comment);
+    addComment();
   };
 
-  return (
-    <>
-      <NavBarInitialDefault />
-      <MKBox
-        minHeight="75vh"
-        width="100%"
-        sx={{
-          backgroundImage: `url(${bgImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "top",
-          display: "grid",
-          placeItems: "center",
-        }}
-      >
+  if (!loading)
+    return (
+      <>
+        <NavBarInitialDefault />
+        <MKBox
+          minHeight="75vh"
+          width="100%"
+          sx={{
+            backgroundImage: `url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <Container>
+            <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+              <MKTypography
+                variant="h1"
+                color="white"
+                mt={-6}
+                mb={1}
+                sx={({ breakpoints, typography: { size } }) => ({
+                  [breakpoints.down("md")]: {
+                    fontSize: size["3xl"],
+                  },
+                })}
+              >
+                {detail.TITRE}
+              </MKTypography>
+            </Grid>
+          </Container>
+        </MKBox>
         <Container>
-          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
-            <MKTypography
-              variant="h1"
-              color="white"
-              mt={-6}
-              mb={1}
-              sx={({ breakpoints, typography: { size } }) => ({
-                [breakpoints.down("md")]: {
-                  fontSize: size["3xl"],
-                },
-              })}
-            >
-              Titre de l&apos;actualité
-            </MKTypography>
-          </Grid>
-        </Container>
-      </MKBox>
-      <Container>
-        <div style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet diam nec
-              justo finibus dictum. Vivamus vitae interdum quam, sit amet scelerisque purus. Nam
-              ultricies sollicitudin gravida. Duis a efficitur sapien. Suspendisse ac laoreet purus.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet diam nec
-              justo finibus dictum. Vivamus vitae interdum quam, sit amet scelerisque purus. Nam
-              ultricies sollicitudin gravida. Duis a efficitur sapien. Suspendisse ac laoreet purus.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet diam nec
-              justo finibus dictum. Vivamus vitae interdum quam, sit amet scelerisque purus. Nam
-              ultricies sollicitudin gravida. Duis a efficitur sapien. Suspendisse ac laoreet purus.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet diam nec
-              justo finibus dictum. Vivamus vitae interdum quam, sit amet scelerisque purus. Nam
-              ultricies sollicitudin gravida. Duis a efficitur sapien. Suspendisse ac laoreet purus.
-            </p>
+          <div style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
+            <div>
+              <p>{detail.DESCRIPTION}</p>
+            </div>
           </div>
-        </div>
-      </Container>
-      <Container sx={{ height: "100%" }}>
-        <Grid container item justifyContent="center" xs={12} lg={6} mx="auto" height="100%">
-          <MKPagination>
-            <MKPagination item>
-              <Icon>keyboard_arrow_left</Icon>
+        </Container>
+        {/* <Container sx={{ height: "100%" }}>
+          <Grid container item justifyContent="center" xs={12} lg={6} mx="auto" height="100%">
+            <MKPagination>
+              <MKPagination item>
+                <Icon>keyboard_arrow_left</Icon>
+              </MKPagination>
+              <MKPagination item active>
+                1
+              </MKPagination>
+              <MKPagination item>2</MKPagination>
+              <MKPagination item>3</MKPagination>
+              <MKPagination item>4</MKPagination>
+              <MKPagination item>5</MKPagination>
+              <MKPagination item>
+                <Icon>keyboard_arrow_right</Icon>
+              </MKPagination>
             </MKPagination>
-            <MKPagination item active>
-              1
-            </MKPagination>
-            <MKPagination item>2</MKPagination>
-            <MKPagination item>3</MKPagination>
-            <MKPagination item>4</MKPagination>
-            <MKPagination item>5</MKPagination>
-            <MKPagination item>
-              <Icon>keyboard_arrow_right</Icon>
-            </MKPagination>
-          </MKPagination>
-        </Grid>
-      </Container>
-      <Container>
-        <div style={{ fontSize: "15px", marginTop: "15px" }}>
-          <span style={{ marginRight: "15px" }}>
-            <img width={"15px"} src={`${process.env.PUBLIC_URL}/reaction.svg`} alt="React icon" />{" "}
-            1234
-          </span>
-          <span>
-            <img
-              width={"15px"}
-              src={`${process.env.PUBLIC_URL}/commentaire.svg`}
-              alt="Comment icon"
-            />{" "}
-            1234
-          </span>
-        </div>
-        <div style={{ marginTop: "15px" }}>
-          <Reacts active={0} hide={!logged} />
-        </div>
-        <div style={{ marginTop: "15px" }}>
-          <h3>Commentaires</h3>
-          <MKBox component="form" role="form" pt={3}>
-            <MKBox mb={2}>
-              <MKInput
-                label="Ecrivez votre commentaire"
-                multiline
-                rows={5}
-                fullWidth
-                value={comment}
-                onChange={(e) => {
-                  setComment(e.target.value);
-                }}
-                disabled={!logged}
-              />
+          </Grid>
+        </Container> */}
+        <Container>
+          <div style={{ fontSize: "15px", marginTop: "15px" }}>
+            {/* <span style={{ marginRight: "15px" }}>
+              <img width={"15px"} src={`${process.env.PUBLIC_URL}/reaction.svg`} alt="React icon" />{" "}
+              1234
+            </span> */}
+            <span>
+              <img
+                width={"15px"}
+                src={`${process.env.PUBLIC_URL}/commentaire.svg`}
+                alt="Comment icon"
+              />{" "}
+              {countComments[0].COUNT}
+            </span>
+          </div>
+          {/* <div style={{ marginTop: "15px" }}>
+            <Reacts active={0} hide={!logged} />
+          </div> */}
+          <div style={{ marginTop: "15px" }}>
+            <h3>Commentaires</h3>
+            <MKBox component="form" role="form" pt={3}>
+              <MKBox mb={2}>
+                <MKInput
+                  label="Ecrivez votre commentaire"
+                  multiline
+                  rows={5}
+                  fullWidth
+                  value={comment}
+                  onChange={(e) => {
+                    setComment(e.target.value);
+                  }}
+                  disabled={!logged}
+                />
+              </MKBox>
+              <MKBox mb={2}>
+                <MKButton variant="gradient" color="info" onClick={onComment} disabled={!logged}>
+                  Commenter
+                </MKButton>
+              </MKBox>
+              {comments.map((comment, i) => (
+                <CommentLine
+                  key={i}
+                  username={comment.UTILISATEUR}
+                  date={comment.DATE_COM}
+                  comment={comment.CONTENU}
+                />
+              ))}
             </MKBox>
-            <MKBox mb={2}>
-              <MKButton variant="gradient" color="info" onClick={onComment} disabled={!logged}>
-                Commenter
-              </MKButton>
-            </MKBox>
-            <CommentLine
-              username={commentData.username}
-              date={commentData.date}
-              comment={commentData.comment}
-            />
-          </MKBox>
-        </div>
-      </Container>
-      <MKBox pt={6} px={1} mt={6}>
-        <DefaultFooter content={footerRoutes} />
-      </MKBox>
-    </>
-  );
+          </div>
+        </Container>
+        <MKBox pt={6} px={1} mt={6}>
+          <DefaultFooter content={footerRoutes} />
+        </MKBox>
+      </>
+    );
+  return <></>;
 }
--- src/pages/Details/PageDetailCallForTenders.js
+++ src/pages/Details/PageDetailCallForTenders.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import Container from "@mui/material/Container";
@@ -7,154 +7,168 @@ import NavBarInitialDefault from "components/NavBarInitialDefault";
 import bgImage from "assets/images/bg-presentation.jpg";
 import DefaultFooter from "examples/Footers/DefaultFooter";
 import footerRoutes from "footer.routes";
-import MKPagination from "components/MKPagination";
-import { Icon, Paper, Button } from "@mui/material";
+// import MKPagination from "components/MKPagination";
+import { Paper } from "@mui/material";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import api from "utils/api";
+import moment from "moment";
 
 PageDetailCallForTenders.propTypes = {
   page: PropTypes.string,
 };
 
 export default function PageDetailCallForTenders() {
-  const callForTenderDetails = {
-    title: "Titre de l'appel d'offre",
-    missions: [
-      "Mission 1",
-      "Mission 2",
-      "Mission 3",
-      // Add more mission items as needed
-    ],
-    requiredProfiles: [
-      "Profile 1",
-      "Profile 2",
-      "Profile 3",
-      // Add more profile items as needed
-    ],
-    companyName: "Nom de la société...",
-    description:
-      "Description de l'appel d'offre. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet diam nec justo finibus dictum. Vivamus vitae interdum quam, sit amet scelerisque purus. Nam ultricies sollicitudin gravida. Duis a efficitur sapien. Suspendisse ac laoreet purus...",
-  };
-  const handleDownloadDocument = () => {
-    // Replace with the logic to download the document
-    // For example, you can use the 'window.open' method to download the file.
-    // Example: window.open('/path/to/document.pdf', '_blank');
+  const { id } = useParams();
+  // eslint-disable-next-line no-unused-vars
+  const [tender, setTender] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getTenderDetail();
+  }, []);
+
+  const getTenderDetail = () => {
+    setLoading(true);
+    fetch(api(`tenders/${id}`)).then((res) => {
+      if (res.ok) {
+        res.json().then((res) => {
+          setTender(res.tender);
+          setLoading(false);
+        });
+      }
+    });
   };
 
-  return (
-    <>
-      <NavBarInitialDefault />
-      <MKBox
-        minHeight="75vh"
-        width="100%"
-        sx={{
-          backgroundImage: `url(${bgImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "top",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+  // const handleDownloadDocument = () => {
+  //   // Replace with the logic to download the document
+  //   // For example, you can use the 'window.open' method to download the file.
+  //   // Example: window.open('/path/to/document.pdf', '_blank');
+  // };
+
+  if (!loading)
+    return (
+      <>
+        <NavBarInitialDefault />
+        <MKBox
+          minHeight="75vh"
+          width="100%"
+          sx={{
+            backgroundImage: `url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Container>
+            <Grid container justifyContent="center" alignItems="center">
+              <MKTypography variant="h1" color="white" mt={-6} mb={1}>
+                {tender.title}
+              </MKTypography>
+            </Grid>
+          </Container>
+        </MKBox>
         <Container>
-          <Grid container justifyContent="center" alignItems="center">
-            <MKTypography variant="h1" color="white" mt={-6} mb={1}>
-              {callForTenderDetails.title}
-            </MKTypography>
+          <Grid container spacing={2} justifyContent="center">
+            {/* <Grid item xs={0}>
+              <Button variant="contained" color="black" onClick={handleDownloadDocument}>
+                Télécharger le document
+              </Button>
+            </Grid> */}
+            <Grid item xs={12}>
+              {/* <Paper sx={{ p: 3 }}>
+                <MKTypography variant="h4" mb={2}>
+                  Nom de la Société
+                </MKTypography>
+                <MKTypography variant="body1" mb={2}>
+                  {callForTenderDetails.companyName}
+                </MKTypography>
+              </Paper> */}
+            </Grid>
+            <Grid item xs={12}>
+              <Paper sx={{ p: 3 }}>
+                <MKTypography variant="h4" mb={2}>
+                  Description
+                </MKTypography>
+                <MKTypography variant="body1" mb={2}>
+                  {tender.description}
+                </MKTypography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Paper sx={{ p: 3 }}>
+                <MKTypography variant="h4" mb={2}>
+                  Critères
+                </MKTypography>
+
+                {tender.critere.map((c, index) => (
+                  <ul key={index}>
+                    <li>
+                      <h5>{c.entitle}</h5>
+                      <p>{c.description}</p>
+                    </li>
+                  </ul>
+                ))}
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Paper sx={{ p: 3 }}>
+                <MKTypography variant="h4" mb={2}>
+                  Dates
+                </MKTypography>
+                <ul>
+                  <li>
+                    <h5>
+                      {"Date d'émission de l'offre: " +
+                        moment(tender.dateEmission).format("DD/MM/YYYY")}
+                    </h5>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <h5>Date limite: {moment(tender.dateLimit).format("DD/MM/YYYY")}</h5>
+                  </li>
+                </ul>
+              </Paper>
+            </Grid>
+
+            {/* <Grid item xs={12}>
+              <Paper sx={{ p: 3 }}>
+                <MKTypography variant="h4" mb={2}>
+                  Instructions de soumission
+                </MKTypography>
+                <MKTypography variant="body1" mb={2}>
+                  {callForTenderDetails.description}
+                </MKTypography>
+              </Paper>
+            </Grid> */}
           </Grid>
         </Container>
-      </MKBox>
-      <Container>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={0}>
-            <Button variant="contained" color="black" onClick={handleDownloadDocument}>
-              Télécharger le document
-            </Button>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper sx={{ p: 3 }}>
-              <MKTypography variant="h4" mb={2}>
-                Nom de la Société
-              </MKTypography>
-              <MKTypography variant="body1" mb={2}>
-                {callForTenderDetails.companyName}
-              </MKTypography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Paper sx={{ p: 3 }}>
-              <MKTypography variant="h4" mb={2}>
-                Mission
-              </MKTypography>
-
-              {callForTenderDetails.missions.map((mission, index) => (
-                <ul key={index}>
-                  <li>
-                    <h5>{mission}</h5>
-                  </li>
-                </ul>
-              ))}
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Paper sx={{ p: 3 }}>
-              <MKTypography variant="h4" mb={2}>
-                Profil Requis
-              </MKTypography>
-
-              {callForTenderDetails.requiredProfiles.map((profile, index) => (
-                <ul key={index}>
-                  <li>
-                    <h5>{profile}</h5>
-                  </li>
-                </ul>
-              ))}
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper sx={{ p: 3 }}>
-              <MKTypography variant="h4" mb={2}>
-                Description et condition
-              </MKTypography>
-              <MKTypography variant="body1" mb={2}>
-                {callForTenderDetails.description}
-              </MKTypography>
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Paper sx={{ p: 3 }}>
-              <MKTypography variant="h4" mb={2}>
-                Instructions de soumission
-              </MKTypography>
-              <MKTypography variant="body1" mb={2}>
-                {callForTenderDetails.description}
-              </MKTypography>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-      <Container sx={{ height: "100%" }}>
-        <Grid container item justifyContent="center" xs={12} lg={6} mx="auto" height="100%">
-          <MKPagination>
-            <MKPagination item>
-              <Icon>keyboard_arrow_left</Icon>
+        {/* <Container sx={{ height: "100%" }}>
+          <Grid container item justifyContent="center" xs={12} lg={6} mx="auto" height="100%">
+            <MKPagination>
+              <MKPagination item>
+                <Icon>keyboard_arrow_left</Icon>
+              </MKPagination>
+              <MKPagination item active>
+                1
+              </MKPagination>
+              <MKPagination item>2</MKPagination>
+              <MKPagination item>3</MKPagination>
+              <MKPagination item>4</MKPagination>
+              <MKPagination item>5</MKPagination>
+              <MKPagination item>
+                <Icon>keyboard_arrow_right</Icon>
+              </MKPagination>
             </MKPagination>
-            <MKPagination item active>
-              1
-            </MKPagination>
-            <MKPagination item>2</MKPagination>
-            <MKPagination item>3</MKPagination>
-            <MKPagination item>4</MKPagination>
-            <MKPagination item>5</MKPagination>
-            <MKPagination item>
-              <Icon>keyboard_arrow_right</Icon>
-            </MKPagination>
-          </MKPagination>
-        </Grid>
-      </Container>
-      <MKBox pt={6} px={1} mt={6}>
-        <DefaultFooter content={footerRoutes} />
-      </MKBox>
-    </>
-  );
+          </Grid>
+        </Container> */}
+        <MKBox pt={6} px={1} mt={6}>
+          <DefaultFooter content={footerRoutes} />
+        </MKBox>
+      </>
+    );
+  else return <></>;
 }
