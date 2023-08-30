019ab3554c52733c5b231744cf09a2c60afcc72d
jheri04 <139941100+jheri04@users.noreply.github.com>
2023-08-31 00:27:11 +0300

    feature profile update profile

--- src/pages/Account/Profile.js
+++ src/pages/Account/Profile.js
@@ -30,91 +30,109 @@ import DefaultNavbar from "examples/Navbars/DefaultNavbar";
 import headerDropdown from "headerDropdown";
 import bgImage from "assets/images/bg-sign-in-basic.jpeg";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "utils/api";
 
 function Profile() {
-  return (
-    <>
-      <MKBox
-        position="absolute"
-        top={0}
-        left={0}
-        zIndex={1}
-        width="100%"
-        minHeight="100vh"
-        sx={{
-          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
-            `${linearGradient(
-              rgba(gradients.dark.main, 0.6),
-              rgba(gradients.dark.state, 0.6)
-            )}, url(${bgImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      />
-      <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={10}>
-        <DefaultNavbar
-          routes={headerDropdown}
-          action={{
-            type: "internal",
-            route: "/pages/Account/Profile",
-            label: "Mon compte",
-            color: "info",
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = () => {
+    setLoading(true);
+    const userSess = JSON.parse(sessionStorage.getItem("user"));
+    fetch(api(`users/${userSess.ID}`)).then((res) => {
+      if (res.ok) {
+        res.json().then((res) => {
+          setUser(res);
+          setLoading(false);
+        });
+      }
+    });
+  };
+
+  if (!loading)
+    return (
+      <>
+        <MKBox
+          position="absolute"
+          top={0}
+          left={0}
+          zIndex={1}
+          width="100%"
+          minHeight="100vh"
+          sx={{
+            backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+              `${linearGradient(
+                rgba(gradients.dark.main, 0.6),
+                rgba(gradients.dark.state, 0.6)
+              )}, url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
           }}
-          transparent
-          light
         />
-        <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
-          <MKBox mt={{ xs: 10, md: 10 }} textAlign="center">
-            <MKAvatar src={profilePicture} alt="Burce Mars" size="xxl" shadow="xl" />
-          </MKBox>
-          <Grid container justifyContent="center" py={6}>
-            <Grid item xs={12} md={7} mx={{ xs: "auto", sm: 6, md: 1 }}>
-              <MKBox display="flex" justifyContent="space-between" alignItems="center" mb={1}>
-                <MKTypography variant="h3">Nom d&apos;utilisateur</MKTypography>
-                <MKBox mx="auto" my={3} textAlign="center">
-                  {/* Link with redirection */}
-                  <Link to="/pages/Account/Parameter">
-                    <MKButton variant="outlined" color="info" size="small">
-                      Paramètre
-                    </MKButton>
-                  </Link>
+        <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={10}>
+          <DefaultNavbar
+            routes={headerDropdown}
+            action={{
+              type: "internal",
+              route: "/pages/Account/Profile",
+              label: "Mon compte",
+              color: "info",
+            }}
+            transparent
+            light
+          />
+          <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
+            <MKBox mt={{ xs: 10, md: 10 }} textAlign="center">
+              <MKAvatar src={profilePicture} alt="Burce Mars" size="xxl" shadow="xl" />
+            </MKBox>
+            <Grid container justifyContent="center" py={6}>
+              <Grid item xs={12} md={7} mx={{ xs: "auto", sm: 6, md: 1 }}>
+                <MKBox display="flex" justifyContent="space-between" alignItems="center" mb={1}>
+                  <MKTypography variant="h3">{`${user.NOM} ${user.PRENOM}`}</MKTypography>
+                  <MKBox mx="auto" my={3} textAlign="center">
+                    {/* Link with redirection */}
+                    <Link to="/pages/Account/Parameter">
+                      <MKButton variant="outlined" color="info" size="small">
+                        Paramètre
+                      </MKButton>
+                    </Link>
+                  </MKBox>
                 </MKBox>
-              </MKBox>
-              <Grid container spacing={3} mb={3}>
-                <Grid item>
-                  <MKTypography component="span" variant="body2" fontWeight="bold">
-                    323&nbsp;
-                  </MKTypography>
-                  <MKTypography component="span" variant="body2" color="text">
-                    Posts
-                  </MKTypography>
-                </Grid>
+                {/* <Grid container spacing={3} mb={3}>
+                  <Grid item>
+                    <MKTypography component="span" variant="body2" fontWeight="bold">
+                      323&nbsp;
+                    </MKTypography>
+                    <MKTypography component="span" variant="body2" color="text">
+                      Posts
+                    </MKTypography>
+                  </Grid>
+                </Grid> */}
+                <MKTypography variant="body1" fontWeight="light" color="text">
+                  Email: {user.EMAIL}
+                </MKTypography>
+                <MKTypography variant="body1" fontWeight="light" color="text">
+                  Role: {user.ROLE}
+                </MKTypography>
+                <MKTypography variant="body1" fontWeight="light" color="text">
+                  Type: {user.TYPE}
+                </MKTypography>
               </Grid>
-              <MKTypography variant="body1" fontWeight="light" color="text">
-                Date de Naissance
-              </MKTypography>
-              <MKTypography variant="body1" fontWeight="light" color="text">
-                Adresse
-              </MKTypography>
-              <MKTypography variant="body1" fontWeight="light" color="text">
-                Genre
-              </MKTypography>
-              <MKTypography variant="body1" fontWeight="light" color="text">
-                Email
-              </MKTypography>
-              <MKTypography variant="body1" fontWeight="light" color="text">
-                Numero de téléphone
-              </MKTypography>
             </Grid>
           </Grid>
-        </Grid>
-      </MKBox>
-      <MKBox pt={6} px={1} mt={6}>
-        <DefaultFooter content={footerRoutes} />
-      </MKBox>
-    </>
-  );
+        </MKBox>
+        <MKBox pt={6} px={1} mt={6}>
+          <DefaultFooter content={footerRoutes} />
+        </MKBox>
+      </>
+    );
+  else return <></>;
 }
 
 export default Profile;
--- src/pages/Account/UpdateInformation.js
+++ src/pages/Account/UpdateInformation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import Container from "@mui/material/Container";
@@ -11,121 +11,159 @@ import MKInput from "components/MKInput";
 import MKButton from "components/MKButton";
 import Card from "@mui/material/Card";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import api from "utils/api";
 
 export default function UpdateInformation() {
-  return (
-    <>
-      <NavBarInitialDefault />
-      <MKBox
-        minHeight="75vh"
-        width="100%"
-        sx={{
-          backgroundImage: `url(${bgImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "top",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Container>
-          <Grid container justifyContent="center" alignItems="center">
-            <MKTypography variant="h1" color="white" mt={-6} mb={1}>
-              Modification des informations
-            </MKTypography>
-          </Grid>
-        </Container>
-      </MKBox>
-      <Container>
-        <Grid container justifyContent="center">
-          <Grid item xs={12} sm={10} md={8} lg={6}>
-            <Card>
-              <MKBox
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-                mx={2}
-                mt={-3}
-                p={2}
-                mb={1}
-                textAlign="center"
-              >
-                <MKButton
-                  variant="gradient"
-                  color="info"
-                  size="small"
-                  startIcon={<ArrowBackIcon />}
-                  onClick={() => {
-                    // Handle the back button click here
-                    // For example, you can use react-router's history.goBack() method
-                    // to go back to the previous page.
-                    // Example: history.goBack();
-                  }}
-                >
-                  Retour
-                </MKButton>
-              </MKBox>
-              <MKBox pt={4} pb={3} px={3}>
-                <MKBox component="form" role="form">
-                  <MKBox mb={2} style={{ display: "flex", gap: "10px" }}>
-                    <MKInput type="text" label="Nom" />
-                    <MKInput type="text" label="Prénom" />
-                  </MKBox>
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [email, setEmail] = useState("");
+  const [pending, setPending] = useState(false);
 
-                  <MKBox mb={2}>
-                    <MKInput
-                      variant="standard"
-                      label="Date de naissance"
-                      InputLabelProps={{ shrink: true }}
-                      type="date"
-                      fullWidth
-                    />
-                  </MKBox>
-                  <MKBox
-                    mb={2}
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "20%",
-                      justifyContent: "center",
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = () => {
+    setLoading(true);
+    const userSess = JSON.parse(sessionStorage.getItem("user"));
+    fetch(api(`users/${userSess.ID}`)).then((res) => {
+      if (res.ok) {
+        res.json().then((res) => {
+          setUser(res);
+          setNom(res.NOM);
+          setPrenom(res.PRENOM);
+          setEmail(res.EMAIL);
+          setLoading(false);
+        });
+      }
+    });
+  };
+
+  const updateUser = () => {
+    setPending(true);
+    fetch(api(`users/${user.ID}`), {
+      headers: { "Content-Type": "application/json" },
+      method: "PUT",
+      body: JSON.stringify({
+        nom,
+        prenom,
+        email,
+      }),
+    }).then((res) => {
+      if (res.ok)
+        res.json().then(() => {
+          fetch(api(`users/${user.ID}`)).then((res) => {
+            if (res.ok) {
+              res.json().then((res) => {
+                delete res.PASSWORD;
+                sessionStorage.setItem("user", JSON.stringify(res));
+                setPending(false);
+              });
+            }
+          });
+        });
+    });
+  };
+
+  if (!loading)
+    return (
+      <>
+        <NavBarInitialDefault />
+        <MKBox
+          minHeight="75vh"
+          width="100%"
+          sx={{
+            backgroundImage: `url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Container>
+            <Grid container justifyContent="center" alignItems="center">
+              <MKTypography variant="h1" color="white" mt={-6} mb={1}>
+                Modification des informations
+              </MKTypography>
+            </Grid>
+          </Container>
+        </MKBox>
+        <Container>
+          <Grid container justifyContent="center">
+            <Grid item xs={12} sm={10} md={8} lg={6}>
+              <Card>
+                <MKBox
+                  variant="gradient"
+                  bgColor="info"
+                  borderRadius="lg"
+                  coloredShadow="info"
+                  mx={2}
+                  mt={-3}
+                  p={2}
+                  mb={1}
+                  textAlign="center"
+                >
+                  <MKButton
+                    variant="gradient"
+                    color="info"
+                    size="small"
+                    startIcon={<ArrowBackIcon />}
+                    onClick={() => {
+                      window.location.href = "/pages/Account/Parameter";
                     }}
                   >
-                    <label style={{ display: "flex", alignItems: "center" }}>
-                      <MKInput type="radio" name="gender" />
-                      <span style={{ marginLeft: "5px", fontSize: "14px", opacity: "7.1" }}>
-                        Homme
-                      </span>
-                    </label>
-                    <label style={{ display: "flex", alignItems: "center" }}>
-                      <MKInput type="radio" name="gender" />
-                      <span style={{ marginLeft: "5px", fontSize: "14px", opacity: "7.1" }}>
-                        Femme
-                      </span>
-                    </label>
-                  </MKBox>
-                  <MKBox mb={2} style={{ display: "flex", gap: "10px" }}>
-                    <MKInput type="text" label="Adresse" />
-                    <MKInput type="number" label="Code postal" />
-                  </MKBox>
-                  <MKBox mb={2} style={{ display: "flex", gap: "10px" }}>
-                    <MKInput type="email" label="Email" />
-                    <MKInput type="number" label="Numero de téléphone" />
-                  </MKBox>
-                  <MKBox mt={4} mb={1}>
-                    <MKButton variant="gradient" color="info" fullWidth>
-                      Modifier
-                    </MKButton>
+                    Retour
+                  </MKButton>
+                </MKBox>
+                <MKBox pt={4} pb={3} px={3}>
+                  <MKBox component="form" role="form">
+                    <MKBox mb={2} style={{ display: "flex", gap: "10px" }}>
+                      <MKInput
+                        type="text"
+                        label="Nom"
+                        value={nom}
+                        onChange={(e) => setNom(e.target.value)}
+                      />
+                      <MKInput
+                        type="text"
+                        label="Prénom"
+                        value={prenom}
+                        onChange={(e) => setPrenom(e.target.value)}
+                      />
+                    </MKBox>
+                    <MKBox mb={2} style={{ display: "flex", gap: "10px" }}>
+                      <MKInput
+                        type="email"
+                        label="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </MKBox>
+                    <MKBox mt={4} mb={1}>
+                      <MKButton
+                        variant="gradient"
+                        color="info"
+                        fullWidth
+                        onClick={updateUser}
+                        disabled={pending}
+                      >
+                        Modifier
+                      </MKButton>
+                    </MKBox>
                   </MKBox>
                 </MKBox>
-              </MKBox>
-            </Card>
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-      <MKBox pt={6} px={1} mt={6}>
-        <DefaultFooter content={footerRoutes} />
-      </MKBox>
-    </>
-  );
+        </Container>
+        <MKBox pt={6} px={1} mt={6}>
+          <DefaultFooter content={footerRoutes} />
+        </MKBox>
+      </>
+    );
+  else return <></>;
 }
